ebc120fb2b8ca8c73a22374e423fbbae479ed0c5
Benny <khatri1732@gmail.com>
2024-04-21 02:08:34 +0530

    init

---
+++ practice/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily:{
+        mullish: ["mullish", "sans-serif"],
+      },
+      colors:{
+        deepBlue: "#02042a",
+        lightBlue: "#2b84ea",
+        lightBlue300: "#4b94ed",
+        lightBlue500: "#0b72e7",
+        lightGreen: "#61cea6",
+      }
+    },
+  },
+  plugins: [],
+}
+
